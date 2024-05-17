547703686d3aefd62e7400af8d666874e5360d2f
mateus leonardo <mateus_leonardo1997@hotmail.com>
2024-05-17 00:00:53 -0300

    feat: adding feed in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <h1 className="title">Dogs next</h1>;
+import photosGet from "@/actions/photos-get";
+import Feed from "@/components/feed/feed";
+
+export default async function Home() {
+  const data = await photosGet();
+  return (
+    <section className="container mainContainer">
+      <Feed photos={data} />
+    </section>
+  );
 }
