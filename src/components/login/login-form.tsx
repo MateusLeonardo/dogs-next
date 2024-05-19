4fc088d25d158fbeeb8d13e164c492b266a0e541
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2024-05-19 17:24:26 -0300

    feat: adding initial loginForm component

---
+++ src/components/login/login-form.tsx
@@ -0,0 +1,13 @@
+import Login from "@/actions/login";
+
+export default function LoginForm() {
+  return (
+    <>
+      <form action={Login}>
+        <input type="text" name="username" placeholder="Usuário" />
+        <input type="password" name="password" placeholder="Senha" />
+        <button type="submit">Entrar</button>
+      </form>
+    </>
+  );
+}
